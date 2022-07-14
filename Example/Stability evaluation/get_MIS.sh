684b1f47f9836cfdea769450a6ef1ccbabb75723
Yinhu <56863457+liyinhu@users.noreply.github.com>
2022-07-14 10:51:09 +0800

    Update and rename get_MIS.sh to Stability_evaluation.example.sh

--- Example/Stability evaluation/get_MIS.sh
+++
@@ -1,2 +0,0 @@
-# calcuate the wMISi for all nodes
-perl ../get_MIS.pl /mnt/Workspace/Project/03.AD_development/01.Relative_abundance/1M_mean.csv /mnt/Workspace/Project/03.AD_development/02.Stability/Relation/AD1M_r.0.05.0.4.csv AD AD1M_MIS.xls
