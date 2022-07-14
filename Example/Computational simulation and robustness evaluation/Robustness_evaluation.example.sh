6e32a827b1e2cc5324cf4714e0ab0182c921543e
Yinhu <56863457+liyinhu@users.noreply.github.com>
2022-07-14 11:47:49 +0800

    Update and rename test to Robustness_evaluation.example.sh

---
+++ Example/Computational simulation and robustness evaluation/Robustness_evaluation.example.sh
@@ -0,0 +1,2 @@
+#Perform simulations of hub and non-hub bacteria removal, evaluate the robustness of GM networks, and repeat 10 times
+perl robustness_evaluate.pl ../Stability evaluation/AD1M_MIS.xls AD1M.bacteria.info AD1M.robustness.xls
